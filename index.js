596004a041ad9aaed178b52e1ce467fd32329e09
Safwan504 <safwanchowdhury7@gmail.com>
2021-06-04 01:25:09 +0600

    second try

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
